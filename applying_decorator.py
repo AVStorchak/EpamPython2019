0344e2e11689cb9f6e312a6d1437169cd120571b
AVStorchak <55753687+AVStorchak@users.noreply.github.com>
2019-12-04 22:19:48 +0300

    Delete applying_decorator.py

--- applying_decorator.py
+++
@@ -1,7 +0,0 @@
-def applydecorator(initial_func):
-    def intermediate_decorator(f):
-        def core_decorator(*args, **kwargs):
-            result = initial_func(f, *args, **kwargs)
-            return result
-        return core_decorator
-    return intermediate_decorator
