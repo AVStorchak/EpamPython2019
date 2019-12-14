3dbf2422ede0e2e69047ecfd2d0f2d169b636fc8
AVStorchak <55753687+AVStorchak@users.noreply.github.com>
2019-12-14 19:23:41 +0300

    Add files via upload

---
+++ 08-networks-basics/tcp_chat_client.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+import threading
+
+
+def receive():
+    while True:
+        try:
+            in_message = c.recv(512).decode("utf8")
+            print(f'{in_message}')
+        except TypeError:
+            break
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 5090))
+threading.Thread(target=receive).start()
+out_message = ''
+
+while out_message != "!quit":
+    out_message = input('[You]: ')
+    client.send(bytes(out_message, "utf8"))
+
+client.close()
