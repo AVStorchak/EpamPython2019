285e589904694fcb6591cef23136a1dcb0a9a350
AVStorchak <55753687+AVStorchak@users.noreply.github.com>
2019-12-23 20:03:30 +0300

    Add files via upload

---
+++ 11-design-patterns/hw/3-observer/dp_observer.py
@@ -0,0 +1,36 @@
+class MyTubeChannel:
+    def __init__(self, channel_name, channel_owner):
+        self.name = channel_name
+        self.owner = channel_owner
+        self.subscribers = []
+        self.videos = []
+        self.playlists = {}
+
+    def subscribe(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def unsubscribe(self, subscriber):
+        if subscriber in self.subscribers:
+            self.subscribers.remove(subscriber)
+
+    def notify_subscribers(self, message):
+        for subscriber in self.subscribers:
+            subscriber.update(message)
+
+    def publish_video(self, video):
+        self.videos.append(video)
+        message = f"there is new video on '{self.name}' channel: '{video}'"
+        self.notify_subscribers(message)
+
+    def publish_playlist(self, playlist):
+        self.playlists.update(playlist)
+        message = f"there is new playlist on '{self.name}' channel: '{list(playlist.keys())[0]}'"
+        self.notify_subscribers(message)
+
+
+class MyTubeUser:
+    def __init__(self, name: str):
+        self._name = name
+
+    def update(self, message):
+        print(f"Dear {self._name}, {message}")
