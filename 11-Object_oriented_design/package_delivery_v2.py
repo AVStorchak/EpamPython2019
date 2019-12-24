1938a362d6c011f5952ef1368d70c9a53c350f94
AVStorchak <55753687+AVStorchak@users.noreply.github.com>
2019-12-24 14:40:23 +0300

    Update package_delivery_v2.py

--- 11-Object_oriented_design/package_delivery_v2.py
+++ 11-Object_oriented_design/package_delivery_v2.py
@@ -4,19 +4,12 @@ import queue
 class BaseWarehouse:
     """
     This is a class for warehouses that operate as dispatch points.
-
     Attributes:
-        fleet: Dictionary of transport units operating from the warehouse,
-        dictionary keys are Transport class objects, dictionary values are
-        actual operation times for such Transport class objects.
-        stock: Queue of packages to be dispatched, each package has
-        a format of [destination, delivery_time], where destination is the
-        code of the final warehouse for the package, delivery_time is
-        absolute time of an operation with the package (delivery to the final or
-        to the transfer warehouse).
+        fleet: List transport units operating from the warehouse,
+        stock: Queue of packages to be dispatched
     """
-    def __init__(self, stock):
-        self.fleet = {}
+    def __init__(self, stock, *transport_units):
+        self.fleet = [*transport_units]
         packages = [[i, 0] for i in stock]
         self.stock = queue.deque(packages)
 
@@ -24,24 +17,24 @@ class BaseWarehouse:
         """
         The function to dispatch a package to the specified address
         by calling the deliver function of the Transport class.
-
         Parameters:
             package: The next package from the queue
             transport: The transport unit is selected from the
             fleet by the lowest time en route.
             address: The address is derived from the dictionary of final
-            warehouses and their codes. If the final warehouse must be
-            reached via a transfer warehouse, such transfer warehouse is
-            selected as the current package address.
+            warehouses and their codes.
         """
-        transport = min(self.fleet, key=self.fleet.get)
+        global final_warehouses
+        
+        self.fleet = sorted(self.fleet, key=lambda x: x.time_en_route)
+        transport = self.fleet[0]
         package = self.stock.popleft()
 
-        if FinalWarehouse.warehouses[package[0]].transfer_warehouse is None or \
-           FinalWarehouse.warehouses[package[0]].transfer_warehouse == self:
-            address = FinalWarehouse.warehouses[package[0]]
+        if final_warehouses[package[0]].transfer_warehouse is None or \
+           final_warehouses[package[0]].transfer_warehouse == self:
+            address = final_warehouses[package[0]]
         else:
-            address = FinalWarehouse.warehouses[package[0]].transfer_warehouse
+            address = final_warehouses[package[0]].transfer_warehouse
 
         transport.deliver(package, address)
 
@@ -52,16 +45,17 @@ class TransferWarehouse(BaseWarehouse):
     do not have initial stock and are located at some distance from base
     warehouses, the __init__ method was changed accordingly.
     """
-    def __init__(self, delivery_time):
+    def __init__(self, delivery_time, *transport_units):
         self.fleet = {}
         self.delivery_time = delivery_time
         self.stock = queue.deque()
+        for unit in transport_units:
+            self.fleet[unit] = unit.time_en_route
 
 
 class FinalWarehouse:
     """
     This is a class for warehouses that are final destinations for packages.
-
     Attributes:
         final_warehouses: Class attribute to keep record of final
         warehouses and their codes.
@@ -71,10 +65,10 @@ class FinalWarehouse:
         warehouse (where available).
         stock: The queue of incoming packages.
     """
-    warehouses = {}
+    global final_warehouses
 
     def __init__(self, code, delivery_time, transfer_warehouse=None):
-        self.warehouses[code] = self
+        final_warehouses[code] = self
         self.delivery_time = delivery_time
         self.transfer_warehouse = transfer_warehouse
         self.stock = queue.deque()
@@ -95,25 +89,17 @@ class Transport:
     """
     This is a class for transport vehicles that operate from base
     warehouses and deliver packages.
-
     Attributes:
         operating_base: The base warehouse to which the transport unit
         is assigned.
         time_en_route: The cumulative transport unit travel time.
     """
-    def __init__(self, operating_base):
-        self.operating_base = operating_base
+    def __init__(self):
         self.time_en_route = 0
-        self.operating_base.fleet[self] = self.time_en_route
 
     def deliver(self, package, address):
         """
         The function to deliver a package to the specified address.
-        If the transport unit has been idle for some period, the parameter is
-        adjusted accordingly using the package delivery time.
-        When the delivery is finished, the fleet status of the transport unit
-        is updated.
-
         Parameters:
             package: Is passed by the base warehouse along with the address.
             delivery_time: Is taken from the destination point and is used to
@@ -127,21 +113,20 @@ class Transport:
         package[1] += address.delivery_time
         address.stock.append(package)
         print(f'Package delivered to {address}, timestamp {package[1]}')
-
         self.time_en_route += 2*address.delivery_time
-        self.operating_base.fleet[self] = self.time_en_route
 
 
 if __name__ == '__main__':
+    final_warehouses = {}
+    truck_1 = Transport()
+    truck_2 = Transport()
+    ship_1 = Transport()
     delivery_order = input("Please provide the delivery order ")
-    factory = BaseWarehouse(delivery_order)
-    port = TransferWarehouse(1)
+    factory = BaseWarehouse(delivery_order, truck_1, truck_2)
+    port = TransferWarehouse(1, ship_1)
     warehouse_a = FinalWarehouse('A', 4, port)
     warehouse_b = FinalWarehouse('B', 5)
-    truck_1 = Transport(factory)
-    truck_2 = Transport(factory)
-    ship_1 = Transport(port)
-
+    
     while factory.stock:
         factory.dispatch_shipment()
 
